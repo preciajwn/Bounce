a084e3efafa9b887509e7af3c92302311b3b0008
preciajwn <precia.juwono@gmail.com>
2021-09-07 20:11:56 +0000

    1 bal stuitert

--- script.js
+++ script.js
@@ -3,12 +3,21 @@
  * de code in deze functie wordt één keer uitgevoerd door
  * de p5 library, zodra het spel geladen is in de browser
  */
+
+ var x = 50;
+ var y = 50;
+
+ var speedY = 5;
+
+ var x1 = 150; 
+ var y1 = 50;
+
+ var speedY1 = 8;
+
 function setup() {
   // Maak een canvas (rechthoek) waarin je je speelveld kunt tekenen
   createCanvas(1280, 720);
 
-  // Kleur de achtergrond blauw, zodat je het kunt zien
-  background('blue');
 }
 
 
@@ -18,9 +27,31 @@ function setup() {
  * uitgevoerd door de p5 library, nadat de setup functie klaar is
  */
 function draw() {
+ // Kleur de achtergrond blauw, zodat je het kunt zien
+ background('blue');
+
   // stel vulkleur in
   fill(100, 100, 255);
 
   // teken een cirkel
-  ellipse(50,50,80,80);
+  ellipse(x,y,80,80);
+  ellipse(x1,y1,80,80);
+  
+  y = y + speedY;
+  y1 = y1 + speedY1;
+
+  if (y === 720) {
+    speedY = speedY * -1;
+  }
+  if (y === 0) {
+    speedY = speedY * -1; 
+  }
+
+
+  if (y1 === 720) {
+    speedY1 = speedY1 * -1;
+  }
+  if (y1 === 0) {
+    speedY1 = speedY1 * -1;
+  }
 }
