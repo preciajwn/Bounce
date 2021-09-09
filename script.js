e5d398bb2176d8ac93443d42a21b1296de541833
preciajwn <precia.juwono@gmail.com>
2021-09-09 17:05:14 +0000

    1 bal tegen alle randen

--- script.js
+++ script.js
@@ -7,12 +7,23 @@
  var x = 50;
  var y = 50;
 
+ var speedX = 2;
  var speedY = 5;
 
- var x1 = 150; 
+ 
+ var x1 = 50;
  var y1 = 50;
 
+ var speedX1 = 4;
  var speedY1 = 8;
+ 
+
+ var x2 = 50;
+ var y2 = 50; 
+
+ var speedX2 = 6;
+ var speedY2 = 12; 
+
 
 function setup() {
   // Maak een canvas (rechthoek) waarin je je speelveld kunt tekenen
@@ -34,24 +45,67 @@ function draw() {
   fill(100, 100, 255);
 
   // teken een cirkel
-  ellipse(x,y,80,80);
-  ellipse(x1,y1,80,80);
+  ellipse(x, y, 80, 80);
+  ellipse(x1, y1, 80, 80);
+  ellipse(x2, y2, 80, 80);
   
+  x = x + speedX;
   y = y + speedY;
+
+  x1 = x1 + speedX1;
   y1 = y1 + speedY1;
+
+  x2 = x2 + speedX2;
+  y2 = y2 + speedY2;
+
 
   if (y === 720) {
     speedY = speedY * -1;
   }
+  
   if (y === 0) {
     speedY = speedY * -1; 
+  }
+
+  if (x === 0) { 
+    speedX = speedX * -1; 
+  }
+
+  if (x === 1280) {
+    speedX = speedX * -1;
   }
 
 
   if (y1 === 720) {
     speedY1 = speedY1 * -1;
   }
+  
   if (y1 === 0) {
-    speedY1 = speedY1 * -1;
+    speedY1 = speedY1 * -1; 
+  }
+
+  if (x1 === 0) { 
+    speedX1 = speedX1 * -1; 
+  }
+
+  if (x1 === 1280) {
+    speedX1 = speedX1 * -1;
+  }
+
+
+  if (y2 === 720) {
+    speedY2 = speedY2 * -1;
+  }
+  
+  if (y2 === 0) {
+    speedY2 = speedY2 * -1; 
+  }
+
+  if (x2 === 0) { 
+    speedX2 = speedX2 * -1; 
+  }
+
+  if (x2 === 1280) {
+    speedX2 = speedX2 * -1;
   }
 }
